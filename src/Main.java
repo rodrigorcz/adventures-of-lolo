6d39b5c16f293feab5820ff8f219966b04f916a4
rodrigorcz <rodrigo.rcastro27@gmail.com>
2023-10-17 16:36:37 -0300

    add images

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import Controler.Tela;
 public class Main {
 
     public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
+        javqa.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 Tela tTela = new Tela();
                 tTela.setVisible(true);
